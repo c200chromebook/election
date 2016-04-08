f08b24af412858fd3284ce31772bf7145946e1a0
mdw <derp@localhost>
2016-04-08 15:21:24 -0700

    whoops forgot samples

--- src/Samples.hs
+++ src/Samples.hs
@@ -4,32 +4,32 @@ import EndToEndElect
 import EvalElection
 --sampleRoster :: Roster
 --sampleRoster = ["cat","dog","chicken"]
-sampleBallot1 :: Ballot
+sampleBallot1 :: Ballot String
 sampleBallot1 = ["chicken","dog"]
-sampleBallot2 :: Ballot
+sampleBallot2 :: Ballot String
 sampleBallot2 = ["dog","cat"]
-sampleBallot3 :: Ballot
+sampleBallot3 :: Ballot String
 sampleBallot3 = ["cat","chicken"]
-sampleBallot4 :: Ballot
+sampleBallot4 :: Ballot String
 sampleBallot4 =  ["chicken","dog","cat"]
-sampleBallot5 :: Ballot
+sampleBallot5 :: Ballot String
 sampleBallot5 = ["budgies","chicken"]
-sampleElection :: Election
+sampleElection :: Election String
 sampleElection = [sampleBallot1,sampleBallot2,sampleBallot3,sampleBallot4,sampleBallot5]
 
 
 
-sampleBallotA :: Ballot
+sampleBallotA :: Ballot String
 sampleBallotA = ["chicken","dog"]
-sampleBallotB :: Ballot
+sampleBallotB :: Ballot String
 sampleBallotB = ["chicken","dog"]
-sampleBallotC :: Ballot
+sampleBallotC :: Ballot String
 sampleBallotC = ["dog","chicken"]
-sampleBallotD :: Ballot
+sampleBallotD :: Ballot String
 sampleBallotD =  ["dog","chicken"]
-sampleBallotE :: Ballot
+sampleBallotE :: Ballot String
 sampleBallotE = ["dog","chicken"] -- only this one should get through, the others should bounce
-sampleElection2 :: Election
+sampleElection2 :: Election String
 sampleElection2 = [sampleBallotA,sampleBallotB,sampleBallotC,sampleBallotD,sampleBallotE]
 
 
@@ -44,9 +44,9 @@ result2 =giveNetworkString sampleElection2
 result3 :: String
 result3 =giveRankedList sampleElectionWC
 
-loopElec :: Election
+loopElec :: Election String
 loopElec = [["A","B"],["B","A"]]
 
-sampleBallot' :: Ballot
+sampleBallot' :: Ballot String
 sampleBallot' = ["chicken","dog","chicken"] -- should ignore final chicken
 result' = giveRankedList [sampleBallot']
